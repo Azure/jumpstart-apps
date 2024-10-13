fd038578302721a80fd63c686f9deea4de4651d7
dkirby-ms <saitcho@outlook.com>
2024-10-13 11:03:55 -0500

    fix for build script

--- agora/main_ui_backend_api/src/pushImages.sh
+++ agora/main_ui_backend_api/src/pushImages.sh
@@ -3,8 +3,9 @@ ACRNAME=agacr5bbaf
 az acr login --name $ACRNAME
 apps=( "main_ui_backend_api/src" "cerebral_api/src" "main_ui/src" "intrusion_detection_api/src" "footfall_ai_api/src" )
 for d in "${apps[@]}" ; do
-    echo "Building $d"
-    docker build -t agora-$d ./$d
-    docker tag agora-$d:latest $ACRNAME.azurecr.io/$d:latest
-    docker push $ACRNAME.azurecr.io/$d:latest
+    e=$(echo $d | tr '_' '-')
+    echo "Building $e"
+    docker build -t agora-$e ./$d
+    docker tag agora-$e:latest $ACRNAME.azurecr.io/$e:latest
+    docker push $ACRNAME.azurecr.io/$e:latest
 done
