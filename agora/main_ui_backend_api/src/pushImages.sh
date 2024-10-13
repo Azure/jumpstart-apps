242a8c08597f47bab5d14b7340989e14de637a9b
dkirby-ms <saitcho@outlook.com>
2024-10-13 11:30:43 -0500

    bug

--- agora/main_ui_backend_api/src/pushImages.sh
+++ agora/main_ui_backend_api/src/pushImages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ACRNAME=agacr5bbaf
 az acr login --name $ACRNAME
-apps=( "main_ui_backend_api/src" "cerebral_api/src" "main_ui/src" "intrusion_detection_api/src" "footfall_ai_api/src" )
+apps=( "main_ui_backend_api" "cerebral_api" "main_ui" "intrusion_detection_api" "footfall_ai_api" )
 for d in "${apps[@]}" ; do
     e=$(echo $d | tr '_' '-')
     echo "Building $e"
-    docker build -t agora-$e ./$d
+    docker build -t agora-$e ./$d/src
     docker tag agora-$e:latest $ACRNAME.azurecr.io/$e:latest
     docker push $ACRNAME.azurecr.io/$e:latest
 done
