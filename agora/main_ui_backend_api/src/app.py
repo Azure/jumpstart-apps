a5b2416ad4c9a9a3569aa3e34dac2f6d5ed8b80b
dkirby-ms <saitcho@outlook.com>
2024-10-15 17:31:31 -0500

    bug

--- agora/main_ui_backend_api/src/app.py
+++ agora/main_ui_backend_api/src/app.py
@@ -409,4 +409,4 @@ class Fridge(Resource):
         return '', 204
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', post=5002)
+    app.run(debug=True, host='0.0.0.0', port=5002)
