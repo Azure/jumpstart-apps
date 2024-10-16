747d6d6041fbd01f5043ca3dfac77b17cec30824
dkirby-ms <saitcho@outlook.com>
2024-10-16 08:03:45 -0500

    remove schema from db init

--- agora/main_ui_backend_api/src/app.py
+++ agora/main_ui_backend_api/src/app.py
@@ -14,7 +14,7 @@ api = Api(app, version='1.0', title='Agora Backend API',
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv('DATABASE_HOST', 'localhost'),
-        database=os.getenv('DATABASE_NAME', 'contoso'),
+        dbname=os.getenv('DATABASE_NAME', 'contoso'),
         user=os.getenv('DATABASE_USER', 'postgres'),
         password=os.getenv('DATABASE_PASSWORD', 'password')
     )
