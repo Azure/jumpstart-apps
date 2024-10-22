be83fded3b2ceaec90dd1e73e7993a7338a42594
dkirby-ms <saitcho@outlook.com>
2024-10-22 14:41:15 -0500

    CORS

--- agora/main_ui_backend_api/src/app.py
+++ agora/main_ui_backend_api/src/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import cameras, zones, regions, hvacs
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(cameras.router)
 app.include_router(zones.router)
